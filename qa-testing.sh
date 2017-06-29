0d5f13412364515c60625da75694e77a5ce91c7e
Peter Uhnak <i.uhnak@gmail.com>
2017-06-29 18:43:41 +0200

    added qa test

---
+++ qa-testing.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eu
+
+
+# SMALLTALK_CI_IMAGE is an env variable available from SmalltalkCI
+# SMALLTALK_VM currently doesn't work, so that's why I search for it by hand
+SMALLTALK_VM="$(find $SMALLTALK_CI_VMS -name pharo -type f -executable | head -n 1) --nodisplay"
+
+run_coverage() {
+	$SMALLTALK_VM $SMALLTALK_CI_IMAGE eval "
+pkg := 'UglyPackage' asPackage.
+
+pkg classes do: [ :cls |
+	Transcript logCr: cls name.
+	cls critiques do: [ :crit |
+		Transcript
+			tab; log: '* '; logCr: crit description
+	].
+
+	Transcript
+		tab; logCr: 'methods:'.
+	cls methods do: [ :meth |
+		Transcript
+			tab; tab; logCr: meth selector.
+		meth critiques do: [ :crit |
+			Transcript
+				tab; tab; tab; log: '* '; logCr: crit description
+		]
+	]
+].
+}
+
+main() {
+	run_coverage
+}
+
+main
